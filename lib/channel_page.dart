bd9617ccbad864ad0f8c89b27f4d3c1c6c3a4ab2
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2017-08-05 17:01:09 -0700

    Add episode list.

--- lib/channel_page.dart
+++ lib/channel_page.dart
@@ -7,15 +7,17 @@ class ChannelPage extends StatelessWidget {
   ChannelPage(this.channel);
   @override
   Widget build(BuildContext context) {
+    var children = [
+      new Hero(tag: channel.image, child: new Image.network(channel.image)),
+      new Text(channel.description)
+    ];
+    children.addAll(channel.episodes.map((e) => new ListTile(title: new Text(e.title))));
     return new Scaffold(
         appBar: new AppBar(title: new Text(channel.title)),
         body: new Container(
             padding: const EdgeInsets.all(16.0),
             child: new ListView(
-              children: <Widget>[
-                new Hero(tag: channel.image, child: new Image.network(channel.image)),
-                new Text(channel.description)
-              ],
+              children: children,
             )
         )
     );
