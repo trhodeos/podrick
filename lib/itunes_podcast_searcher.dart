343d666b127dbe02863b37cd96b70adfda25d037
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2017-08-09 18:55:01 -0700

    Basic itunes searching working.. Hooking up UI next.

--- lib/itunes_podcast_searcher.dart
+++ lib/itunes_podcast_searcher.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:json_object/json_object.dart';
 import 'package:flutter/services.dart';
 import 'package:uri/uri.dart';
 
@@ -22,20 +22,75 @@ abstract class SearchResults {
   Iterable<SearchResult> results;
 }
 
-class _SearchResultImpl extends JsonObject implements SearchResult {
-  _SearchResultImpl();
+class _SearchResultImpl implements SearchResult {
 
-  factory _SearchResultImpl.fromJsonString(string) {
-    return new JsonObject.fromJsonString(string, new _SearchResultImpl());
+  _SearchResultImpl(this.artistName, this.artworkUrl100, this.artworkUrl30,
+      this.artworkUrl60, this.artworkUrl600, this.collectionName, this.feedUrl,
+      this.genres, this.kind, this.trackCount);
+
+  factory _SearchResultImpl.fromMap(object) {
+    return new _SearchResultImpl(
+        object['artistName'],
+        object['artworkUrl100'],
+        object['artworkUrl30'],
+        object['artworkUrl60'],
+        object['artworkUrl600'],
+        object['collectionName'],
+        object['feedUrl'],
+        object['genres'],
+        object['kind'],
+        object['trackCount']);
   }
+  @override
+  String artistName;
+
+  @override
+  String artworkUrl100;
+
+  @override
+  String artworkUrl30;
+
+  @override
+  String artworkUrl60;
+
+  @override
+  String artworkUrl600;
+
+  @override
+  String collectionName;
+
+  @override
+  String feedUrl;
+
+  @override
+  Iterable<String> genres;
+
+  @override
+  String kind;
+
+  @override
+  int trackCount;
 }
 
-class _SearchResultsImpl extends JsonObject implements SearchResults {
-  _SearchResultsImpl();
+class _SearchResultsImpl  implements SearchResults {
 
-  factory _SearchResultsImpl.fromJsonString(string) {
-    return new JsonObject.fromJsonString(string, new _SearchResultsImpl());
+  _SearchResultsImpl(this.results, this.resultsCount);
+
+  factory _SearchResultsImpl.fromMap(object) {
+    var resultsCount = object['resultCount'];
+    var results = <_SearchResultImpl>[];
+    for (var s in object['results']) {
+      results.add(new _SearchResultImpl.fromMap(s));
+    }
+
+    return new _SearchResultsImpl(results, resultsCount);
   }
+
+  @override
+  Iterable<SearchResult> results;
+
+  @override
+  int resultsCount;
 }
 
 
@@ -44,7 +99,12 @@ final _template = new UriTemplate("https://itunes.apple.com/search{?term,entity}
 Future<SearchResults> queryItunes(String term) async {
    var client = createHttpClient();
    var url = _template.expand({'term': term, 'entity': 'podcast'});
-   return client.get(url).then((response) {
-      return _SearchResultImpl.fromJsonString(response.body);
+   print("Querying itunes for term: " + term);
+   print(url);
+   return client.read(url).then((body) {
+     JsonCodec codec = new JsonCodec();
+     Map<String, dynamic> object = codec.decode(body);
+     SearchResults results = new _SearchResultsImpl.fromMap(object);
+     return results;
    });
 }
