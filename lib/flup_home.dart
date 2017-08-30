6d375d823cb60222199084b4bdfcd981bb333a03
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2017-08-29 20:57:33 -0700

    Get firebase DB working. Also reformat.

--- lib/flup_home.dart
+++ lib/flup_home.dart
@@ -3,15 +3,27 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'itunes_podcast_searcher.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 
 final googleSignIn = new GoogleSignIn();
+final auth = FirebaseAuth.instance;
+final libraryReference = FirebaseDatabase.instance.reference().child('library');
 
 Future<Null> _ensureLoggedIn() async {
   GoogleSignInAccount user = googleSignIn.currentUser;
-  if (user == null)
-    user = await googleSignIn.signInSilently();
+  if (user == null) user = await googleSignIn.signInSilently();
   if (user == null) {
     await googleSignIn.signIn();
+  }
+  if (auth.currentUser == null) {
+    GoogleSignInAuthentication credentials =
+        await googleSignIn.currentUser.authentication;
+    await auth.signInWithGoogle(
+      idToken: credentials.idToken,
+      accessToken: credentials.accessToken,
+    );
   }
 }
 
@@ -27,6 +39,7 @@ class FlupHomePage extends StatefulWidget {
 class TabChoice {
   final String title;
   final IconData icon;
+
   const TabChoice({this.title, this.icon});
 }
 
@@ -42,8 +55,17 @@ class PodcastChannelWidget extends StatelessWidget {
   final String imageUrl;
   final String rssUrl;
 
-  PodcastChannelWidget({this.title, this.imageUrl="http://via.placeholder.com/100x100", this.rssUrl});
+  PodcastChannelWidget(
+      {this.title,
+      this.imageUrl = "http://via.placeholder.com/100x100",
+      this.rssUrl});
 
+  factory PodcastChannelWidget.createForSnapshot(snapshot) {
+    return new PodcastChannelWidget(
+        title: snapshot.value['title'],
+        imageUrl: snapshot.value['imageUrl'],
+        rssUrl: snapshot.value['rssUrl']);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,17 +75,18 @@ class PodcastChannelWidget extends StatelessWidget {
           child: new Container(
               height: 56.0,
               child: new Row(children: [
-                new Hero(tag: title, child: new Image.network(imageUrl, fit: BoxFit.contain)),
+                new Hero(
+                    tag: title,
+                    child: new Image.network(imageUrl, fit: BoxFit.contain)),
                 new Container(
                     margin: const EdgeInsets.only(left: 16.0),
                     child: new Text(title)),
               ])),
-          onTap: () =>
-              Navigator.pushNamed(context, '/channel:${Uri.encodeQueryComponent(rssUrl)}'),
+          onTap: () => Navigator.pushNamed(
+              context, '/channel:${Uri.encodeQueryComponent(rssUrl)}'),
         ));
   }
 }
-
 
 class LibraryWidget extends StatefulWidget {
   @override
@@ -71,23 +94,20 @@ class LibraryWidget extends StatefulWidget {
 }
 
 class _LibraryWidgetState extends State<LibraryWidget> {
-
   @override
   Widget build(BuildContext context) {
     _ensureLoggedIn();
-    return new ListView(
-      shrinkWrap: true,
+    return new Column(
       children: <Widget>[
-        new PodcastChannelWidget(
-          title: "Hidden Brain",
-          rssUrl: "https://www.npr.org/rss/podcast.php?id=510308"),
-        new PodcastChannelWidget(
-          title: "Planet Money",
-          rssUrl: "https://www.npr.org/rss/podcast.php?id=510289" /* planet money */),
-        new PodcastChannelWidget(
-          title: "Invisibilia",
-          rssUrl: "https://www.npr.org/rss/podcast.php?id=510307" /* invisibilia */,
-        )
+        new Flexible(
+            child: new FirebaseAnimatedList(
+          query: libraryReference,
+          sort: (a, b) => b.key.compareTo(a.key),
+          padding: new EdgeInsets.all(8.0),
+          itemBuilder: (_, DataSnapshot snapshot, Animation<double> animation) {
+            return new PodcastChannelWidget.createForSnapshot(snapshot);
+          },
+        )),
       ],
     );
   }
@@ -99,7 +119,6 @@ class SearchWidget extends StatefulWidget {
 }
 
 class _SearchWidgetState extends State<SearchWidget> {
-
   SearchResults results;
 
   _updateSearch(s) async {
@@ -111,59 +130,52 @@ class _SearchWidgetState extends State<SearchWidget> {
 
   @override
   Widget build(BuildContext context) {
-    var children =  <Widget> [
+    var children = <Widget>[
       new TextField(
-        maxLines: 1,
-        autocorrect: false,
-        keyboardType: TextInputType.text,
-        decoration: new InputDecoration(icon: new Icon(Icons.search)),
-        onChanged: (s) {
-          _updateSearch(s);
-        })
+          maxLines: 1,
+          autocorrect: false,
+          keyboardType: TextInputType.text,
+          decoration: new InputDecoration(icon: new Icon(Icons.search)),
+          onChanged: (s) {
+            _updateSearch(s);
+          })
     ];
 
     if (results != null) {
       for (var result in results.results) {
         children.add(new PodcastChannelWidget(
-          title: result.collectionName,
-          imageUrl: result.getBestArtwork(),
-          rssUrl: result.feedUrl
-        ));
+            title: result.collectionName,
+            imageUrl: result.getBestArtwork(),
+            rssUrl: result.feedUrl));
       }
     }
 
-    return new Container(
-        child: new ListView(
-          children: children));
+    return new Container(child: new ListView(children: children));
   }
 }
-
-
 
 class _FlupHomePageState extends State<FlupHomePage> {
   @override
   Widget build(BuildContext context) {
     return new DefaultTabController(
-    length: choices.length,
-    child: new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-        bottom: new TabBar(
-          isScrollable: false,
-          tabs: choices.map((choice) {
-            return new Tab(text: choice.title, icon: new Icon(choice.icon));
-          }).toList(),
-        )
-      ),
-      body: new TabBarView(
-        children: choices.map((choice) {
-          if (choice.title == "Library") {
-            return new LibraryWidget();
-          } else if (choice.title == "Find") {
-            return new SearchWidget();
-          }
-        }).toList()
-      )
-    ));
+        length: choices.length,
+        child: new Scaffold(
+            appBar: new AppBar(
+                title: new Text(widget.title),
+                bottom: new TabBar(
+                  isScrollable: false,
+                  tabs: choices.map((choice) {
+                    return new Tab(
+                        text: choice.title, icon: new Icon(choice.icon));
+                  }).toList(),
+                )),
+            body: new TabBarView(
+                children: choices.map((choice) {
+              if (choice.title == "Library") {
+                return new LibraryWidget();
+              } else if (choice.title == "Find") {
+                return new SearchWidget();
+              }
+            }).toList())));
   }
 }
